6acf7bea143e297de7254a5b0d5724de586083fe
Matheus Tadeu <mtqr1@hotmail.com>
2019-03-24 03:15:22 -0300

    Add Screen

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles'
+
+const Main = (props) => (
+    <View>
+        <Text>Main Screen</Text>
+    </View>
+);
+
+export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+
+    display: flex;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+`;
